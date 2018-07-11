1e1a6e6432d28eba1c592c70e75ed8ee56f2974c
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-07-11 19:16:02 +1000

    Use of ES6 template strings to render entire HTML components

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,45 @@
 // Global app controller
 import Search from './models/Search';
+import * as searchView from './views/searchView';
+import { elements } from './views/base';
 
 global._babelPolyfill = false;
 
-const searchObj= new Search('pizza');
-searchObj.getResults();
+/** Global state of the app
+ * - Search object
+ * - Current recipe object
+ * - Shopping list object
+ * - Liked recipes
+ */
+const state = {};
+
+/** 
+ * SEARCH CONTROLLER
+ */
+// Call back function 
+const controlSearch = async () => {
+    // 1. Get the query from view
+    const query = searchView.getInput();
+
+    if (query) {
+        // 2) New search object and add to search member of state variable.
+        state.search = new Search(query);
+
+        // 3) Prepare UI for results
+        searchView.clearInput();
+        searchView.clearResults();
+
+        // 4) Search for recipes
+        await state.search.getResults();
+
+        // 5) Render results on UI
+        searchView.renderResults(state.search.result);
+    }
+};
+
+// Add a listener to the search command button 
+document.querySelector('.search').addEventListener('submit', el => {
+    // Prevent the default action like page reload,etc
+    el.preventDefault();
+    controlSearch();
+});
