01d183bb0f1ca1731a1b3fe1c41e45d25356b47a
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-07-11 20:02:52 +1000

    Custom method to format title to show limitedd characters

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -9,6 +9,30 @@ export const clearInput = () => elements.searchInput.value = '';
 // Clear the results page
 export const clearResults = () => elements.searchResList.innerHTML = '';
 
+/* Testing - limitRecipeTitle function with sample data
+// 'Pasta with tomato and spinach'
+acc: 0 / acc + cur.length = 5 / newTitle = ['Pasta']
+acc: 5 / acc + cur.length = 9 / newTitle = ['Pasta', 'with']
+acc: 9 / acc + cur.length = 15 / newTitle = ['Pasta', 'with', 'tomato']
+acc: 15 / acc + cur.length = 18 / newTitle = ['Pasta', 'with', 'tomato', 'and']
+acc: 18 / acc + cur.length = 24 / newTitle = ['Pasta', 'with', 'tomato', 'and']
+*/
+// Method to format the title to show limited char -> so that its displayed one line on our page
+export const limitRecipeTitle = (title, limit = 10) => {
+    const newTitle = [];
+    if (title.length > limit) {
+        title.split(' ').reduce((acc, cur) => {
+            if (acc + cur.length <= limit) {
+                newTitle.push(cur);
+            }
+            return acc + cur.length;
+        }, 0);
+        // return the result
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
+}
+
 // Load the recipes on to the UI
 const renderRecipe = recipe => {
     // Form the HTML markup with all the received values
@@ -19,7 +43,7 @@ const renderRecipe = recipe => {
                 <img src="${recipe.image_url}" alt="${recipe.title}">
             </figure>
             <div class="results__data">
-                <h4 class="results__name">${recipe.title}</h4>
+                <h4 class="results__name">${limitRecipeTitle(recipe.title,15)}</h4>
                 <p class="results__author">${recipe.publisher}</p>
             </div>
         </a>
