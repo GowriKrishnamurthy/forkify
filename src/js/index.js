b2ab1730c77632ecbe0b44c97e233c463e30b5cd
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2018-07-11 16:02:07 +1000

    Making the first API call to foodtofork.com

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 // Global app controller
+import axios from 'axios';
+
+global._babelPolyfill = false;
+
+// Making API calls to food2fork.com 
+async function getResults(query) {
+    const proxy = 'https://cors-anywhere.herokuapp.com/';
+    const key = 'd9116bf99e3acef6866daf871a8cd0a6';
+
+    try {
+        const res = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+        const recipes = res.data.recipes;
+        console.log(recipes);
+    }
+    catch (error) {
+        alert(error);
+    }
+}
+getResults('pizza');
